19bf2d70d90ef774170620a6a74418a603acbf41
milimetric <dandreescu@wikimedia.org>
2017-06-13 11:05:36 -0400

    Refactor the way merge works

--- test/DimensionalData.spec.js
+++ test/DimensionalData.spec.js
@@ -24,39 +24,40 @@ const pageviews3 = [
 describe('DimensionalData', function () {
     it('should be able to return its crossfilter instance', function () {
         let dim = new DimensionalData();
+
         expect(dim.getCrossfilter().add).not.toBeUndefined();
     });
 
     it('should return the sum value for a set of records', function () {
         let dim = new DimensionalData(pageviews1);
+
         expect(dim.total('views')).toEqual(100);
     });
 
     it('should merge results', function () {
         let dim = new DimensionalData(pageviews1)
+
         dim.merge(pageviews2)
         expect(dim.total('views')).toEqual(110)
     });
 
     it('should break down by any column', function () {
         let dim = new DimensionalData(pageviews1)
-        let merged = dim.merge(pageviews2)
 
         dim.measure('views')
-        merged.measure('views')
-
         expect(dim.breakdown('date')['2017-01']).toEqual(10)
-        expect(merged.breakdown('date')['2017-01']).toEqual(11)
+
+        dim.merge(pageviews2)
+        expect(dim.breakdown('date')['2017-01']).toEqual(11)
     });
 
     it('should break down by two columns in order', function () {
         let dim = new DimensionalData(pageviews1)
-        let merged = dim.merge(pageviews2)
 
-        merged.measure('views')
-
-        expect(merged.breakdown('date', 'agent')['2017-01']['spider']).toEqual(1)
-        expect(merged.breakdown('agent', 'date')['spider']['2017-01']).toEqual(1)
+        dim.merge(pageviews2)
+        dim.measure('views')
+        expect(dim.breakdown('date', 'agent')['2017-01']['spider']).toEqual(1)
+        expect(dim.breakdown('agent', 'date')['spider']['2017-01']).toEqual(1)
     });
 
     it('should report unique values in a column', function () {
