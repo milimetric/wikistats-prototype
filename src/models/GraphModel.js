a831e6c703653d5d412cc1b88cc4c3eeaed4242b
Francisco Dans <fdans@wikimedia.org>
2017-06-26 14:46:27 +0200

    makes graphmodel return data with breakdowns

--- src/models/GraphModel.js
+++ src/models/GraphModel.js
@@ -7,14 +7,31 @@ class GraphModel {
         const xAxisValue = 'timestamp';
         const yAxisValue = 'views';
         this.dimensionalData.measure('timestamp');
-        const rawValues = this.dimensionalData.breakdown('views');
-        return rawValues.map((row) => {
-            var ts = row.timestamp.slice(1);
-            const month = ts.slice(0,4) + '-'
-                    + ts.slice(4,6) + '-'
-                    + ts.slice(6,8)
-            return {month: month, total: row.views}
-        })
+        const activeBreakdown = this.metricData.breakdowns.filter((breakdown) => {
+            return breakdown.values.some((v) => {
+                return v.on
+            });
+        })[0];
+        if (activeBreakdown) {
+            let brokenDownValues = [];
+            const rawValues = this.dimensionalData.breakdown(yAxisValue, activeBreakdown.breakdownName);
+            return rawValues.map((row) => {
+                var ts = row.timestamp;
+                const month = ts.slice(0,4) + '-'
+                        + ts.slice(4,6) + '-'
+                        + ts.slice(6,8)
+                return {month: month, total: row.views}
+            });
+        } else {
+            const rawValues = this.dimensionalData.breakdown(yAxisValue);
+            return rawValues.map((row) => {
+                var ts = row.timestamp;
+                const month = ts.slice(0,4) + '-'
+                        + ts.slice(4,6) + '-'
+                        + ts.slice(6,8)
+                return {month: month, total: row.views}
+            });
+        }
     }
     getBreakdowns () {
         return this.metricData.breakdowns;
