11fe4d9b08cd11fe4f0482e52dd944da11131e4e
Francisco Dans <fdans@wikimedia.org>
2017-06-27 12:55:47 +0200

    moves breakdowns to its own component

---
+++ src/components/detail/Breakdowns.vue
@@ -0,0 +1,32 @@
+<template>
+<div v-if="breakdowns">
+    <div class="ui clearing divider"></div>
+
+    <h3 class="header">Filters and Breakdowns</h3>
+
+    <div v-for="b in breakdowns">
+        {{b.name}}
+        <label class="xui checkbox" v-for="bv in b.values">
+            <input type="checkbox" v-model="bv.on"/>
+            {{bv.name}}
+        </label>
+
+        <div class="ui toggle checkbox">
+            <input type="checkbox" id="breakdown" v-model="b.on">
+            <label for="breakdown">
+                Breakdown
+                <span v-if="!b.on">Off</span>
+                <span v-if="b.on">On</span>
+                <i class="help circle icon" title="Breakdowns help you see more detail by breaking down the total values into parts."/>
+            </label>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'breakdowns',
+        props: ['breakdowns']
+    }
+</script>
--- src/components/detail/DetailSidebar.vue
+++ src/components/detail/DetailSidebar.vue
@@ -20,40 +20,20 @@
         <a @click.prevent="viewMoreMetrics" href="#">View more metrics</a>
     </p>
 
-    <div v-if="breakdowns">
-    <div class="ui clearing divider"></div>
-
-        <h3 class="header">Filters and Breakdowns</h3>
-
-        <div v-for="b in breakdowns">
-            {{b.name}}
-            <label class="xui checkbox" v-for="bv in b.values">
-                <input type="checkbox" v-model="bv.on"/>
-                {{bv.name}}
-            </label>
-
-            <div class="ui toggle checkbox">
-                <input type="checkbox" id="breakdown" v-model="b.on">
-                <label for="breakdown">
-                    Breakdown
-                    <span v-if="!b.on">Off</span>
-                    <span v-if="b.on">On</span>
-                    <i class="help circle icon" title="Breakdowns help you see more detail by breaking down the total values into parts."/>
-                </label>
-            </div>
-        </div>
-    </div>
+    <breakdowns :breakdowns='breakdowns'/>
 </section>
 </template>
 
 <script>
 import WikiSelector from '../WikiSelector'
+import Breakdowns from './Breakdowns'
 
 export default {
     name: 'detail-sidebar',
     props: ['wiki','otherMetrics','metric','breakdowns','area'],
     components: {
-        WikiSelector
+        WikiSelector,
+        Breakdowns
     },
     methods: {
         wikiSelected (wiki) {
